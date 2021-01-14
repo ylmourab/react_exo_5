a4c56dd419f6f3a49e71aae503060a47e5f85740
Younes <ylmourab77@gmail.com>
2021-01-14 13:19:49 +0100

    fin exo 5

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Profil nom="Primo" prenom="Nicolas">description de Nicolas</Profil>
       <Profil nom="Turkish" prenom="kadir">description de Kadir</Profil>
       <Profil nom="Albert" prenom="hacker">description de albert</Profil>
+      <Profil nom="Oussari" prenom="Ibrahim"></Profil>
      
     </div>
   );
--- src/components/Profil.js
+++ src/components/Profil.js
@@ -6,7 +6,7 @@ export class Profil extends Component {
             <div>
                 <h1>{this.props.prenom}</h1>
                 <h2>{this.props.nom}</h2>
-                <p>{this.props.children}</p>
+                <p>{this.props.children ? this.props.children : `pas de description` }</p>
             </div>
         )
     }
