30252f5cda41cd20bbd625cc7b3e3310ac98b9d2
Younes <ylmourab77@gmail.com>
2021-01-14 12:24:34 +0100

    profil fait dans le app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Profil from './components/Profil';
+
+function App() {
+  return (
+    <div className="App">
+      <Profil/>
+      <Profil/>
+      <Profil/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Profil.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Profil extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Nicola</h1>
+                <h2>Primo</h2>
+                <p>coach modeste</p>
+            </div>
+        )
+    }
+}
+
+export default Profil
